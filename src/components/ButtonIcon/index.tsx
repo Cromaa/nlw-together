fe6a5c36dcf2714e8308c7a7166bc52492d2da75
Cromaa <dev.cromaa@gmail.com>
2021-06-21 11:24:33 -0300

    Liftoff concluded with sucess

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    Text,
+    Image,
+    View,
+    TouchableOpacity,
+    TouchableOpacityProps
+} from 'react-native';
+
+import DiscordImg from '../../assets/discord.png'
+import { styles } from './styles'
+
+//type or interface
+//? opçoes não obrigatorias
+
+type props = TouchableOpacityProps & {
+    title: string;
+}
+
+export function ButtonIcon({ title, ...rest } : props){
+    return (
+        <TouchableOpacity 
+            style = {styles.container}
+            { ...rest }
+        >
+            <View style = {styles.iconWrapper}>
+                <Image 
+                    source = {DiscordImg} 
+                    style = {styles.icon}
+                />
+            </View>
+
+            <Text style = {styles.title}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}

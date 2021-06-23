653b737e5d862e75f6c4d3fef20aa8f07e7f9af9
Cromaa <dev.cromaa@gmail.com>
2021-06-23 17:54:25 -0300

    feat: Form

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { Text } from 'react-native';
+
+import { styles } from './styles'
+
+type props = RectButtonProps & {
+    title: string;
+}
+
+export function Button({ title, ...rest } : props){
+    return (
+        <RectButton 
+            style = {styles.container}
+            { ...rest }
+        >
+            <Text style = {styles.title}>
+                {title}
+            </Text>
+        </RectButton>
+    )
+}
--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
 
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
@@ -11,48 +11,103 @@ import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 import { CategorySelect } from '../../components/CategorySelect';
 import { GuildIcon } from '../../components/GuildIcon';
+import { SmallInput } from '../../components/SmallInput';
+import { TextArea } from '../../components/TextArea';
+import { Button } from '../../components/Button';
 
 export function AppointmentCreate(){
     const [category, setCategory] = useState('');
     return (
-        <Background>
-            <Header 
-                title = 'Agendar partida'
-            />
-            <Text style = {[
-                styles.label, 
-                {marginLeft: 24, marginTop: 36, marginBottom: 18}
-            ]}>
-                Categoria
-            </Text>
+        <KeyboardAvoidingView 
+            behavior = {Platform.OS === 'ios' ? 'padding' : 'height'}
+            style = {styles.container}
+        >
+            <ScrollView>
+                    <Header 
+                        title = 'Agendar partida'
+                    />
+                    <Text style = {[
+                        styles.label, 
+                        {marginLeft: 24, marginTop: 36, marginBottom: 18}
+                    ]}>
+                        Categoria
+                    </Text>
 
-            <CategorySelect 
-                hasCheckBox
-                setCategory = {setCategory}
-                categorySelected = {category}
-            />
+                    <CategorySelect 
+                        hasCheckBox
+                        setCategory = {setCategory}
+                        categorySelected = {category}
+                    />
 
-            <View style = {styles.form}>
-                <RectButton>
-                    <View style = {styles.select}>
-                        {
-                            // <View style = {styles.image}/>
-                            <GuildIcon />
-                        }
-                        <View style = {styles.selectBody}>
-                            <Text style = {styles.label}>
-                                Selecione um servidor
-                            </Text>
+                    <View style = {styles.form}>
+                        <RectButton>
+                            <View style = {styles.select}>
+                                {
+                                    // <View style = {styles.image}/>
+                                    <GuildIcon />
+                                }
+                                <View style = {styles.selectBody}>
+                                    <Text style = {styles.label}>
+                                        Selecione um servidor
+                                    </Text>
+                                </View>
+
+                                <Feather 
+                                    name = "chevron-right"
+                                    color = {theme.colors.heading}
+                                    size = {18}
+                                />
+                            </View>
+                        </RectButton>
+
+                        <View style = {styles.field}>
+                            <View>
+                                <Text style = {styles.label}>
+                                    Dia e mês
+                                </Text>
+                                <View style = {styles.column}>
+                                    <SmallInput maxLength = {2}/>
+                                    <Text style = {styles.divider}>
+                                        /
+                                    </Text>
+                                    <SmallInput maxLength = {2}/>
+                                </View> 
+                            </View>
+                            <View>
+                                <Text style = {styles.label}>
+                                    Horário
+                                </Text>
+                                <View style = {styles.column}>
+                                    <SmallInput maxLength = {2}/>
+                                    <Text style = {styles.divider}>
+                                        :
+                                    </Text>
+                                    <SmallInput maxLength = {2}/>
+                                </View> 
+                            </View>
                         </View>
 
-                        <Feather 
-                            name = "chevron-right"
-                            color = {theme.colors.heading}
-                            size = {18}
-                        />
+                            <View style = {[styles.field, {marginBottom: 12}]}>
+                                <Text style = {styles.label}>
+                                    Descrição
+                                </Text>
+                                <Text style = {styles.caracterLimit}>
+                                    Max 100 caracteres
+                                </Text>
+                            </View>
+
+                            <TextArea 
+                                multiline
+                                maxLength = {100}
+                                numberOfLines = {5}
+                                autoCorrect = {false}
+                            />
+
+                            <View style = {styles.footer}>
+                                <Button title = 'Agendamento'/>
+                            </View>
                     </View>
-                </RectButton>
-            </View>
-        </Background>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }

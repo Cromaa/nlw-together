76c4cd74ba3af53d3a80b32b1cf80305e2751729
Cromaa <dev.cromaa@gmail.com>
2021-06-25 10:34:53 -0300

    feat: Application data persisted

---
+++ src/configs/database.ts
@@ -0,0 +1,11 @@
+const DATABASE_NAME = '@gameplay';
+
+//@nomeapp: coleção
+
+const COLLECTION_USERS = `${DATABASE_NAME}:user`;
+const COLLECTION_APPOITMENTS = `${DATABASE_NAME}:appointments`;
+
+export {
+    COLLECTION_USERS,
+    COLLECTION_APPOITMENTS
+}
